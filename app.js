89b4b653fe8d0c7c8974d68c1c63933a1f2a8fd4
Pavel919 <65679951+Pavel919@users.noreply.github.com>
2020-08-28 16:33:58 +0300

    Update app.js

--- app.js
+++ app.js
@@ -16,15 +16,30 @@ $(document).ready(function(){
         variableWidth: true, /*задает свою ширину слик слайдера*/
     })
 
+  $(document).ready(function(){
 
-    $(document).ready(function(){
-        var show = true;
-        var countbox = ".resultat";
+    var show = true;
+    var countbox = ".resultat";
 
-    $('.numnum').spincrement({
-        duration: 2500
-        });
+    $(window).on("scroll load resize", function(){
+        if (!show) return false; // Отменяем показ анимации, если она уже была выполнена
+        var w_top = $(window).scrollTop(); // Количество пикселей на которое была прокручена страница
+        var e_top = $(countbox).offset().top; // Расстояние от блока со счетчиками до верха всего документа
+        var w_height = $(window).height(); // Высота окна браузера
+        var d_height = $(document).height(); // Высота всего документа
+        var e_height = $(countbox).outerHeight(); // Полная высота блока со счетчиками
+        if (w_top + 500 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height){
+
+            $('.numnum').spincrement({
+                thousandSeparator: "",
+                duration: 3500
+            });
+
+            show = false;
+        }
     });
+
+});
 
 
     $(window).scroll(function(){
